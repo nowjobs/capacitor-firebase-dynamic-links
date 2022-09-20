4fcfa177dadf4bb0987485c9236bcf5a146194cf
Blake Ward <26913683+Nevvulo@users.noreply.github.com>
2022-09-20 10:48:24 +1000

    Update Android implementation to Capacitor 4

--- android/src/main/java/com/joinflux/flux/firebasedynamiclinks/FirebaseDynamicLinksPlugin.java
+++ android/src/main/java/com/joinflux/flux/firebasedynamiclinks/FirebaseDynamicLinksPlugin.java
@@ -1,29 +1,27 @@
-package com.joinflux.flux.capacitorfirebasedynamiclinks;
+package com.joinflux.flux.firebasedynamiclinks;
+
+import static android.content.ContentValues.TAG;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
+import androidx.annotation.NonNull;
 import com.getcapacitor.JSObject;
-import com.getcapacitor.annotation.CapacitorPlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
 import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import static android.content.ContentValues.TAG;
-
-@CapacitorPlugin()
-public class CapacitorFirebaseDynamicLinks extends Plugin {
+@CapacitorPlugin(name = "FirebaseDynamicLinks")
+public class FirebaseDynamicLinksPlugin extends Plugin {
 
     private static final String EVENT_DEEP_LINK = "deepLinkOpen";
 
-    public void load () {
+    public void load() {
         Log.println(Log.DEBUG, "DynamicLinks", "plugin loaded");
     }
 
@@ -31,9 +29,12 @@ public class CapacitorFirebaseDynamicLinks extends Plugin {
     protected void handleOnNewIntent(Intent intent) {
         super.handleOnNewIntent(intent);
 
-        FirebaseDynamicLinks.getInstance()
-                .getDynamicLink(intent)
-                .addOnSuccessListener(getActivity(), new OnSuccessListener<PendingDynamicLinkData>() {
+        FirebaseDynamicLinks
+            .getInstance()
+            .getDynamicLink(intent)
+            .addOnSuccessListener(
+                getActivity(),
+                new OnSuccessListener<PendingDynamicLinkData>() {
                     @Override
                     public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
                         Uri deepLink = null;
@@ -48,12 +49,16 @@ public class CapacitorFirebaseDynamicLinks extends Plugin {
                             notifyListeners(EVENT_DEEP_LINK, ret, true);
                         }
                     }
-                })
-                .addOnFailureListener(getActivity(), new OnFailureListener() {
+                }
+            )
+            .addOnFailureListener(
+                getActivity(),
+                new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.w(TAG, "getDynamicLink:onFailure", e);
                     }
-                });
+                }
+            );
     }
 }
