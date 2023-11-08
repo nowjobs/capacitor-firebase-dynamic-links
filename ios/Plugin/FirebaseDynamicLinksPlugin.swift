5df02109cb3231849f7ee2b18eebfe5c3a6ce116
Rafael Nicdao <nicdaoraf@gmail.com>
2023-11-08 14:18:40 +1100

    Avoid using the Capacitor universal link notification name

--- ios/Plugin/FirebaseDynamicLinksPlugin.swift
+++ ios/Plugin/FirebaseDynamicLinksPlugin.swift
@@ -5,18 +5,22 @@ import FirebaseDynamicLinks
 
 typealias JSObject = [String:Any]
 
+extension Notification.Name {
+    public static let firebaseDynamicLink = Notification.Name(rawValue: "FirebaseDynamicLink")
+}
+
 @objc(CapacitorFirebaseDynamicLinks)
 public class CapacitorFirebaseDynamicLinks: CAPPlugin {
-    
+
     public override func load() {
         if (FirebaseApp.app() == nil) {
             FirebaseApp.configure()
         }
 
         NotificationCenter.default.addObserver(self, selector: #selector(self.handleUrlOpened(notification:)), name: Notification.Name.capacitorOpenURL, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(self.handleUniversalLink(notification:)), name: Notification.Name.capacitorOpenUniversalLink, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.handleUniversalLink(notification:)), name: Notification.Name.firebaseDynamicLink, object: nil)
     }
-    
+
     @objc func handleUrlOpened(notification: NSNotification) {
         guard let object = notification.object as? [String:Any?] else {
             return
@@ -25,12 +29,12 @@ public class CapacitorFirebaseDynamicLinks: CAPPlugin {
         guard let dynamicLink = DynamicLinks.dynamicLinks().dynamicLink(fromCustomSchemeURL: object["url"] as! URL) else {
             return
         }
-            
+
         let params = dynamicLink.url?.query ?? ""
         let path = dynamicLink.url?.path ?? ""
         self.sendNotification(path: path, params: params)
     }
-    
+
     @objc func handleUniversalLink(notification: NSNotification) {
         guard let object = notification.object as? [String:Any?] else {
             return
@@ -42,7 +46,7 @@ public class CapacitorFirebaseDynamicLinks: CAPPlugin {
             self.sendNotification(path: path, params: params)
         }
     }
-    
+
     func sendNotification(path: String, params: String) {
         let data = ["slug": path, "query": params ]
         self.notifyListeners("deepLinkOpen", data: data, retainUntilConsumed: true)
